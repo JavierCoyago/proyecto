7144bd512ed06fb25a85a283a1a75b3a0b55cba6
JavierCoyago <jxcoyago@gmail.com>
2024-05-03 22:32:04 -0500

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Ejemplo de función para agregar un evento click a un botón
+const btnInscripcion = document.getElementById("inscripcion");
+
+btnInscripcion.addEventListener("click", () => {
+    alert("¡Gracias por tu interés! Te contactaremos pronto.");
+});
